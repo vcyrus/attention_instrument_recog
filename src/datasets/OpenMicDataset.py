316f03b17df0e978630932a3b4e48a14426593d4
Cyrus <cyrusvahidi@gmail.com>
2020-04-28 18:19:07 +0100

    model and dataset loader

--- src/datasets/OpenMicDataset.py
+++ src/datasets/OpenMicDataset.py
@@ -8,20 +8,36 @@
 
 import os
 
+import numpy as np
+
 import torchaudio
 
 from src.datasets.Dataset import Dataset
 
 class OpenMicDataset(Dataset):
-    def __init__(self, 
-                path):
+    '''
+        path: path to npz file
+    '''
+    def __init__(self, path):
         self.path = path
 
-    def walk_path(self):
-        raise NotImplementedError
+        self.load_openmic()
+
+    def load_openmic(self):
+        np_load_old = np.load
+        # modify the default parameters of np.load
+        np.load = lambda *a,**k: np_load_old(*a, allow_pickle=True, **k)
+
+        openmic = np.load(os.path.join(self.path, 'openmic-2018.npz'))
+
+        np.load = np_load_old
+
+        self.features = openmic['X']
+        self.labels = openmic['Y_true']
+        self.sample_keys = openmic['sample_key']
 
     def __getitem__(self, idx):
         raise NotImplementedError
 
     def __len__(self):
-        return len(self.files)
+        return len(self.features)
