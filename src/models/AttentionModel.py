fix attention softmax dimensions and loss function BCEwLogits

src/models/AttentionModel.py
@@ -19,13 +19,16 @@ class AttentionModel(Model):
         self.fc2 = nn.Linear(embedding_dim, embedding_dim)
         self.fc3 = nn.Linear(embedding_dim, embedding_dim)
 
-        self.attn = AttentionLayer(temporal_dim, n_classes)
-        self.pred = nn.Linear(embedding_dim, n_classes)
+        self.attn = AttentionLayer(embedding_dim, n_classes)
+        self.score = nn.Linear(embedding_dim, n_classes)
         
         self.dropout = nn.Dropout(dropout_rate)
-        self.bn1 = nn.BatchNorm1d(embedding_dim)
-        self.bn2 = nn.BatchNorm1d(embedding_dim)
-        self.bn3 = nn.BatchNorm1d(embedding_dim)
+        self.bn1 = nn.BatchNorm1d(temporal_dim)
+        self.bn2 = nn.BatchNorm1d(temporal_dim)
+        self.bn3 = nn.BatchNorm1d(temporal_dim)
+
+
+        self.n_classes = n_classes
 
     def forward(self, batch):
         z = self.dropout(self.bn1(F.relu(self.fc1(batch))))
@@ -34,20 +37,20 @@ class AttentionModel(Model):
 
         embedding = torch.add(batch, z)
 
-        score = self.attn(torch.tranpose(embedding, 1, 2))
-        pred = self.pred(embedding)
+        attn_weights = self.attn(embedding)
+        score = self.score(embedding)
 
-        y = nn.Sigmoid(torch.mul(score, pred))
+        score = torch.sum(torch.mul(attn_weights, score), dim=1)
+        import pdb; pdb.set_trace()
 
-        return y
+        return score.view((-1, self.n_classes))
 
 class AttentionLayer(nn.Module):
     def __init__(self, n_dims_in, n_dims_out):
-        super(AttentionModel, self).__init__()
-
-        self.score = nn.Linear(n_dims_in, n_dims_out)
+        super(AttentionLayer, self).__init__()
+        self.attn = nn.Linear(n_dims_in, n_dims_out)
     
-    def forward(self, batch):
-        score = nn.Softmax(self.score(torch.transpose(embedding, 1, 2)))
+    def forward(self, z):
+        weights = torch.matmul(z, self.attn.weight.t())
         
-        return score
+        return F.softmax(weights, dim=2)

src/scripts/train.py
@@ -22,7 +22,6 @@ if __name__ == "__main__":
                                   'openmic-2018-aggregated-labels.csv'))
     label_to_idx = {label: idx 
               for idx, label in enumerate(np.sort(df['instrument'].unique()))}
-
     
     datasets = get_datasets(
                   args.openmic_path, 
@@ -38,10 +37,12 @@ if __name__ == "__main__":
                                       else 'cpu')
     print('Device: {}'.format(cuda_device))
 
-    model = AttentionModel()
+    model = AttentionModel(n_classes, 
+                          embedding_dim=args.embedding_dim,
+                          dropout_rate=args.dropout_rate)
 
     optimizer = Adam(model.parameters(), args.lr, weight_decay=args.weight_decay)
-    criterion = nn.BCELoss(reduction='sum')
+    criterion = nn.BCEWithLogitsLoss(reduction='sum')
 
     model, criterion = train(
         datasets, 
@@ -53,7 +54,6 @@ if __name__ == "__main__":
         optimizer,
         writer,
         args.weight_decay, 
-        args.audio_len,
         criterion,
         args.patience,
     )
@@ -63,12 +63,5 @@ if __name__ == "__main__":
     if args.out_path:
         if not os.path.exists(args.out_path):
             os.mkdir(args.out_path)
-        path = os.path.join(args.out_path, 
-                          "model_{0}".format(args.transform))
+        path = os.path.join(args.out_path, "model")
         torch.save(model.state_dict(), path)
-
-    confusions = evaluate(model, 
-                          cuda_device, 
-                          datasets['test'], 
-                          label_to_idx, 
-                          criterion)

src/utils/training.py
@@ -18,7 +18,6 @@ def train(datasets,
           optimizer, 
           writer, 
           weight_decay, 
-          audio_len_s, 
           criterion, 
           patience): 
     '''
@@ -28,15 +27,11 @@ def train(datasets,
       lr         -> float, learning rate
       use_cuda   -> bool, use gpu
       out_path   -> str, path to write the model to
-      fs         -> int, audio sampling rate
-      win_length -> int, feature extraction window length (ms)
-      hop_length -> int, feature extraction hop length (ms)
       n_bins     -> int, feature extraction number of mel bands
       n_classes  -> int, number of classification classes
       writer     -> torch.utils.tensorboard.SummaryWriter
       dropout_rate -> float, dropout rate for model
       weight_decay -> float, loss regularization weight decay
-      audio_len_s  -> int, length of the input audio in seconds
     '''
     params_train = {
         "batch_size": batch_size,
@@ -50,7 +45,6 @@ def train(datasets,
   
     model.to(cuda_device)
 
-    criterion = nn.CrossEntropyLoss()
     optimizer = Adam(model.parameters(), lr, weight_decay=weight_decay)
     scheduler = lr_scheduler.ReduceLROnPlateau(optimizer, factor=0.3)
     early_stopping = EarlyStopping(patience=patience, verbose=True)
@@ -93,9 +87,9 @@ def run_epoch(model, criterion, optimizer, generator, cuda_device, phase=None):
     model = model.train() if phase =='train' else model.eval()
     for i, batch in enumerate(generator, 0):
         
-        X, y = batch["audio"], batch["label"]
-        X = X.to(device=cuda_device)
-        y = y.to(device=cuda_device)
+        X, y = batch["features"], batch["labels"]
+        X = X.to(device=cuda_device).float()
+        y = y.to(device=cuda_device).float()
 
         _y = model(X)
         loss = criterion(_y, y)